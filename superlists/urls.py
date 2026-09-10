6afcee3a3db6cedf16ba328a9b42008f69a30f90
Mariana <marianaaribeiroo98@gmail.com>
2026-09-10 11:37:24 -0300

    Conectando a view home_page nas URLs do projeto

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from lists import views
+from lists.views import home_page
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', home_page, name='home'),
+    # path('admin/', admin.site.urls), # (pode deixar o admin comentado ou ativo se preferir)
 ]
