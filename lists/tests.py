e2ff16fbfc314821a094dfe333526ab0d8ef7ce6
Mariana <marianaaribeiroo98@gmail.com>
2026-09-10 14:50:20 -0300

    Redirecionando após o POST

--- lists/tests.py
+++ lists/tests.py
@@ -2,12 +2,20 @@ from django.test import TestCase
 from lists.models import Item
 
 class HomePageTest(TestCase):
-    
-    ...
-    
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
 class ItemModelTest(TestCase):
 
