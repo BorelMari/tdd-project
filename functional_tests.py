2d9690019ee67579b43c392a1918b3d17f2830ff
Mariana <marianaaribeiroo98@gmail.com>
2026-09-10 13:37:56 -0300

    Melhorando a Página Inicial

--- functional_tests.py
+++ functional_tests.py
@@ -42,3 +42,7 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    self.assertTrue(
+    any(row.text == '1: Estudar testes funcionais' for row in rows),
+    "New to-do item did not appear in table"
+)
